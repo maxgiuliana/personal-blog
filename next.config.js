361f3e3bf31aa882d4f8c1a6748bcb11d45a3941
Max Giuliana <64877544+CapCoderr@users.noreply.github.com>
2025-07-23 13:50:10 -0400

    Fix build configuration for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'dist',
   images: {
     unoptimized: true
   },
   basePath: process.env.NODE_ENV === 'production' ? '/personal-blog' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/personal-blog/' : ''
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/personal-blog/' : '',
+  trailingSlash: true
 }
 
 module.exports = nextConfig
