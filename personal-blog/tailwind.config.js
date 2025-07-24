dad4b91f0a62d98214ab2203b4a2eb1b69b660f5
maxgiuliana <your.email@example.com>
2025-07-23 20:02:17 -0400

    Add project setup and configuration

---
+++ personal-blog/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,md,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,md,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
